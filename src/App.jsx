6a20410d6e493efcdb22b07ca55efde3e430a3f7
R0F7 <wwwrafikhan075@gmail.com>
2025-02-25 19:25:36 +0600

    add Product Details section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Banner from "./component/Banner/Banner";
 import Navbar from "./component/Navbar/Navbar";
+import ProductDetails from "./component/ProductDetails/ProductDetails";
 
 const App = () => {
   return (
     <main>
       <Navbar></Navbar>
       <Banner></Banner>
+      <ProductDetails></ProductDetails>
     </main>
   );
 };
--- src/component/Button/Button.jsx
+++ src/component/Button/Button.jsx
@@ -1,19 +1,21 @@
 import { GoArrowRight } from "react-icons/go";
 import PropTypes from "prop-types";
 
-const Button = ({ text, px, py }) => {
-console.log(px,py);
+const Button = ({ text, px, py, width }) => {
+  console.log(px, py, width);
 
   return (
     <div>
       <button
-        className={`flex items-center gap-2.5 bg-[#FF6600] text-white px-${px} py-${py} rounded-full px-0`}
+        className={`flex items-center gap-2.5 bg-[#FF6600] text-white rounded-full px-0 ${
+          width ? "w-full flex justify-center" : "w-auto"
+        }`}
         style={{
-          paddingLeft:`${px}`,
-          paddingRight:`${px}`,
-          paddingTop:`${py}`,
-          paddingBottom:`${py}`,          
-          }}
+          paddingLeft: `${px}`,
+          paddingRight: `${px}`,
+          paddingTop: `${py}`,
+          paddingBottom: `${py}`,
+        }}
       >
         {text}
         <i>
@@ -25,9 +27,9 @@ console.log(px,py);
 };
 
 Button.propTypes = {
-    text: PropTypes.string,
-    px:PropTypes.string,
-    py:PropTypes.string,
-  };
+  text: PropTypes.string,
+  px: PropTypes.string,
+  py: PropTypes.string,
+};
 
 export default Button;
